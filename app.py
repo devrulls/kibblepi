37c4ac954be988e47f9856555b7105ec14ff82ff
Raul Hernandez <devrulls@gmail.com>
2022-03-03 10:15:33 +0100

    first commit App-Cat

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title('Welcome to Cat Kibble Box -CKB')
+
+status = st.header('What would you like to do? ')
+hobby = st.selectbox("Kibble-Box: ",
+                     ['Box001', 'Box002', 'Box003'])
+
+st.write("Your hobby is: ", hobby)
+
+hobbies = st.multiselect("Options available: ",
+                         ['Camera', 'Email', 'Eat'])
+
+st.write("You selected", len(hobbies), 'options')
+
+
+if st.button('Submit'):
+    st.success("Successful operation!!!!!")
