d5d702171ecb7e18b2a37ee18273e12bf5e9228f
ZamalLeFourbe <samuel.rochette06@gmail.com>
2022-03-10 15:39:19 +0100

    Add a first test

---
+++ pi_server/test/addition/test_addition.py
@@ -0,0 +1,10 @@
+from app.addition.addition import add
+
+
+class TestAddition:
+    def test_add_10_and_3_returns_13(self):
+        # When
+        result = add(10, 3)
+
+        # Then
+        assert result == 13
