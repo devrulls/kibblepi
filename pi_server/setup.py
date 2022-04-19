d0dc987615a4df185323dc15e6461b61f77768e3
Samos <samuel.rochette06@gmail.com>
2022-04-19 23:25:35 +0200

    fix CI

--- pi_server/setup.py
+++ pi_server/setup.py
@@ -4,7 +4,7 @@ setup(
     name="pi_server",
     version="0.1.0",
     packages=find_packages(exclude=["tests"]),
-    install_requires=["flask==2.0.3", "RPi.GPIO==0.7.1"],
+    install_requires=["flask==2.0.3"],#, "RPi.GPIO==0.7.1"],
     extras_require={"dev": ["pytest", "mypy", "black"]},
     entry_points={
         "console_scripts": [
