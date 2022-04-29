ebe4a669b164c57a25a3282a197b4df3e9644e68
Raul Hernandez <devrulls@gmail.com>
2022-04-29 17:57:39 +0200

    Update Dockerfile

--- webapp/setup.py
+++ webapp/setup.py
@@ -4,7 +4,7 @@ setup(
     name="webapp",
     version="0.1.0",
     packages=find_packages(exclude=["tests"]),
-    install_requires="streamlit==1.7.0",
+    install_requires="streamlit==1.8.1",
     extras_require={"dev": ["pytest", "mypy", "black"]},
     entry_points={
         "console_scripts": [
